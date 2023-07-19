996b4bfd9b91dc24058e551b12946aaedc76ff01
talasago <42575556+talasago@users.noreply.github.com>
2023-07-19 23:37:24 +0900

    style: コメント追加

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import Engine
 
 # TODO:本番環境の設定を別途検討する必要あり
-# TODO:alembic.iniと共通化した方が良さそう
+# TODO:alembic.iniと共通化した方が良さそう。環境変数とかかなあ
 SQLALCHEMY_DATABASE_URL: str = "postgresql://postgres:postgres_password@localhost:5432/postgres"
 
 engine: Engine = create_engine(SQLALCHEMY_DATABASE_URL)
