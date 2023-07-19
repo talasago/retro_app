780bfaf3e6dff44cc82f474741a160a45a95bc26
talasago <42575556+talasago@users.noreply.github.com>
2023-07-19 23:37:08 +0900

    test: fixtureでDBを削除→作成→テスト実行→DB接続セッション削除するように変更

--- test/conftest.py
+++ test/conftest.py
@@ -2,21 +2,54 @@
 # TODO:テストコードのファイル毎に書くのはめんどいので共通化したい気持ち
 import sys
 import os
-app_path = os.path.join(os.path.dirname(__file__), '../')
-sys.path.append(app_path)
-print(app_path)
+pj_root_path = os.path.join(os.path.dirname(__file__), '../')
+sys.path.append(pj_root_path)
 
 
 import pytest  # noqa: E402
 from sqlalchemy.orm import Session  # noqa: E402
-from app.database import SessionLocal  # noqa: E402
+from alembic import command  # noqa: E402
+from alembic.config import Config  # noqa: E402
+from app.database import SessionLocal, Base  # noqa: E402
+from sqlalchemy import create_engine  # noqa: E402
 
 
-@pytest.fixture(scope="module")
+# TODO:AutoUseをtrueにしたい
+# TODO:関数名をdb_sessionにしたい
+@pytest.fixture(scope="session")
 def db() -> Session:
-    """データベースセッションのフィクスチャ"""
+    """データベースセッションのフィクスチャ。DBを削除→作成→テスト実行→DB接続セッション削除している"""
+
+    # NOTE:TBL削除→TBL作成→→テスト実行できるようにすることで、テストデータを毎回削除する手間を減らしている。
+    # ロジックはこちらのHPを参考にした
+    # https://nikaera.com/archives/pytest-sqlalchemy-alembic/
+
+    # TODO:環境変数とかにしたほうがいいかも
+    TEST_SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres_password@localhost:5432/postgres"
+
+    engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL)
+
+    # テーブルの作成
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    # NOTE:これマイグレートしてる意味あるのかな。よくわかってない。多分意味ない.
+    # ホントはDBを消すべきなのだろうけど。`ERROR: cannot drop the currently open database`となって削除できない
+    # https://www.postgresql.jp/document/7.3/reference/sql-dropdatabase.htmlを見ると
+    # >対象とするデータベースに接続している場合、このコマンドを実行することができません。とあるので、
+    # 時的に別のデータベースに接続し、テスト用のデータベースをドロップすれば行けるのかもしれない。
+    # 時間かかりそうなので一旦後回し。
+    migrate()
+
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+        engine.dispose()
+
+
+def migrate() -> None:
+    # マイグレーションの実行
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
