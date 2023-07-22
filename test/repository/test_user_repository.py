98b4f4f7b8cec2a48e0d98732e7ca4e7bacd6500
talasago <42575556+talasago@users.noreply.github.com>
2023-07-22 18:12:09 +0900

    refactor: 不要コード削除とコメント修正

--- test/repository/test_user_repository.py
+++ test/repository/test_user_repository.py
@@ -4,8 +4,6 @@ import sys
 import os
 app_path = os.path.join(os.path.dirname(__file__), '../..')
 sys.path.append(app_path)
-print(app_path)
-
 
 import pytest  # noqa: E402
 from sqlalchemy.orm import Session   # noqa: E402
@@ -18,13 +16,16 @@ from app.schema.user import UserCreate  # noqa: E402
 class TestUserRepository:
     def test_create_user(self, db: Session):
         # テストデータの作成
-        user_data = UserCreate(name="John Doe", email="johndoe@example.com", password="password")
+        user_data = UserCreate(
+            name="John Doe", email="johndoe@example.com", password="password")
 
         # UserRepositoryのインスタンスを作成
         user_repo = UserRepository(db)
 
         # create_userメソッドを呼び出してユーザーを作成
         created_user = user_repo.create_user(user_data)
+
+        # 日付のassertが欲しい。日本時間かどうか。
 
         # ユーザーが正しく作成されたかを検証
         assert created_user.name == user_data.name
