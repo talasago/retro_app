8763f3e223e54172d64e35e93f4e95b6e1004349
talasago <42575556+talasago@users.noreply.github.com>
2023-07-22 20:47:33 +0900

    feat: passwordのハッシュ化処理を追加

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -1,3 +1,4 @@
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from ..models.user import User
 from ..schema.user import UserCreate
@@ -8,10 +9,11 @@ class UserRepository:
         self.db = db
 
     def create_user(self, user_params: UserCreate) -> User:
-        # TODO:パスワードの暗号化が必要
-        hashed_password = user_params.password + 'notreallyhashed'
+        hashed_password = self.__generate_hashed_password(
+            plain_password=user_params.password)
         user_params = User(name=user_params.name, email=user_params.email,
                            hashed_password=hashed_password)
+
         self.db.add(user_params)
         self.db.commit()
         self.db.refresh(user_params)
@@ -19,3 +21,7 @@ class UserRepository:
 
     def get_user(self, user_id: int) -> User:
         return self.db.query(User).get(user_id)
+
+    def __generate_hashed_password(self, plain_password) -> str:
+        pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+        return pwd_context.hash(plain_password)
--- test/repository/test_user_repository.py
+++ test/repository/test_user_repository.py
@@ -8,25 +8,24 @@ sys.path.append(app_path)
 import pytest  # noqa: E402
 from sqlalchemy.orm import Session   # noqa: E402
 from app.repository.user_repository import UserRepository  # noqa: E402
-# from app.models.user import User # noqa: E402
 from app.schema.user import UserCreate  # noqa: E402
+from passlib.context import CryptContext  # noqa: E402
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 @pytest.mark.usefixtures('db')
 class TestUserRepository:
     def test_create_user(self, db: Session):
-        # テストデータの作成
         user_data = UserCreate(
             name='John Doe', email='johndoe@example.com', password='password')
+        sut = UserRepository(db)
 
-        # UserRepositoryのインスタンスを作成
-        user_repo = UserRepository(db)
+        created_user = sut.create_user(user_data)
 
-        # create_userメソッドを呼び出してユーザーを作成
-        created_user = user_repo.create_user(user_data)
-
-        # 日付のassertが欲しい。日本時間かどうか。
-
-        # ユーザーが正しく作成されたかを検証
+        # ユーザーが正しく作成されたか検証
+        # FIXME:DBの値を見に行く必要がある
+        # TODO:日付のassertが欲しい。日本時間かどうか。
         assert created_user.name == user_data.name
         assert created_user.email == user_data.email
+        assert pwd_context.verify('password', created_user.hashed_password)
