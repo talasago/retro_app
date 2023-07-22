3d1f2224f3dd14300b9a478d3ac593c32b40bd13
talasago <42575556+talasago@users.noreply.github.com>
2023-07-22 21:23:28 +0900

    refactor: SQLAlchemyとpydanticのクラス名を変更

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -1,26 +1,26 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from ..models.user import User
-from ..schema.user import UserCreate
+from ..models.user_model import UserModel
+from ..schema.user_schema import UserCreate
 
 
 class UserRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_user(self, user_params: UserCreate) -> User:
+    def create_user(self, user_params: UserCreate) -> UserModel:
         hashed_password = self.__generate_hashed_password(
             plain_password=user_params.password)
-        user_params = User(name=user_params.name, email=user_params.email,
-                           hashed_password=hashed_password)
+        user_params = UserModel(name=user_params.name, email=user_params.email,
+                                hashed_password=hashed_password)
 
         self.db.add(user_params)
         self.db.commit()
         self.db.refresh(user_params)
         return user_params  # TODO:何もリターンしなくていいと思う
 
-    def get_user(self, user_id: int) -> User:
-        return self.db.query(User).get(user_id)
+    def get_user(self, user_id: int) -> UserModel:
+        return self.db.query(UserModel).get(user_id)
 
     def __generate_hashed_password(self, plain_password) -> str:
         pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
--- test/repository/test_user_repository.py
+++ test/repository/test_user_repository.py
@@ -8,8 +8,9 @@ sys.path.append(app_path)
 import pytest  # noqa: E402
 from sqlalchemy.orm import Session   # noqa: E402
 from app.repository.user_repository import UserRepository  # noqa: E402
-from app.schema.user import UserCreate  # noqa: E402
+from app.schema.user_schema import UserCreate  # noqa: E402
 from passlib.context import CryptContext  # noqa: E402
+from app.models.user_model import UserModel  # noqa: E402
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -21,11 +22,13 @@ class TestUserRepository:
             name='John Doe', email='johndoe@example.com', password='password')
         sut = UserRepository(db)
 
-        created_user = sut.create_user(user_data)
+        sut.create_user(user_data)
 
         # ユーザーが正しく作成されたか検証
-        # FIXME:DBの値を見に行く必要がある
         # TODO:日付のassertが欲しい。日本時間かどうか。
+        created_user: UserModel = db.query(UserModel) \
+            .filter_by(email='johndoe@example.com').first()
+
         assert created_user.name == user_data.name
         assert created_user.email == user_data.email
         assert pwd_context.verify('password', created_user.hashed_password)
