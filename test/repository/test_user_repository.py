9488d707abce141dde58eee253b7c05798ef3011
talasago <42575556+talasago@users.noreply.github.com>
2023-07-23 13:09:44 +0900

    test: PATHの処理を削除

--- test/repository/test_user_repository.py
+++ test/repository/test_user_repository.py
@@ -1,16 +1,9 @@
-# appディレクトリの実行パスを追加
-# TODO:テストコードのファイル毎に書くのはめんどいので共通化したい気持ち
-import sys
-import os
-app_path = os.path.join(os.path.dirname(__file__), '../..')
-sys.path.append(app_path)
-
-import pytest  # noqa: E402
-from sqlalchemy.orm import Session   # noqa: E402
-from app.repository.user_repository import UserRepository  # noqa: E402
-from app.schemas.user_schema import UserCreate  # noqa: E402
-from passlib.context import CryptContext  # noqa: E402
-from app.models.user_model import UserModel  # noqa: E402
+import pytest
+from sqlalchemy.orm import Session
+from app.repository.user_repository import UserRepository
+from app.schemas.user_schema import UserCreate
+from passlib.context import CryptContext
+from app.models.user_model import UserModel
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
