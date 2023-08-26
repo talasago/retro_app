bce3ad41fe66b6822f1deb8e812812ac1df7fbdd
talasago <42575556+talasago@users.noreply.github.com>
2023-08-26 23:47:58 +0900

    test: test_user作成処理を共通化

---
+++ tests/test_helpers/create_test_user.py
@@ -0,0 +1,21 @@
+from factories.user_factory import CommonUserFactory
+
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.repository.user_repository import UserRepository
+    from app.models.user_model import UserModel
+
+
+# TODO:UserRepositoryを引数で受け取るのではなく、fixtureで定義したuser_repoを呼び出せ無いものか
+def create_test_user(user_repo: 'UserRepository', email: str | None = None,
+                     password: str | None = None, name: str | None = None) -> 'UserModel':
+    test_user_param = {}
+
+    test_user_param.setdefault('email', email) if email else None
+    test_user_param.setdefault('password', password) if password else None
+    test_user_param.setdefault('name', name) if name else None
+    test_user = CommonUserFactory(**test_user_param)
+    user_repo.save(test_user)
+
+    return test_user
