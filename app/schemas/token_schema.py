0a39b403342b789d9b76cd476b5fa3ece8d295cb
talasago <42575556+talasago@users.noreply.github.com>
2023-08-27 22:10:45 +0900

    feat: expect_token_typeが既定の値ではない場合例外を返す

--- app/schemas/token_schema.py
+++ app/schemas/token_schema.py
@@ -1,11 +1,17 @@
+from enum import Enum
 from pydantic import BaseModel
 from datetime import datetime
+
+
+class TokenType(Enum):
+    access_token = 'access_token'
+    refresh_token = 'refresh_token'
 
 
 class TokenPayload(BaseModel):
     """Tokenの中身のモデルクラス(pydantic)"""
 
-    token_type: str
+    token_type: str  # TODO:クラスを指定する
     exp: int | datetime  # decodeするときに必ずintになる
     uid: str
     jti: str
