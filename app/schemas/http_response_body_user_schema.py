154109f050583307fb796730b48247c25575a5a0
talasago <42575556+talasago@users.noreply.github.com>
2023-09-09 21:00:14 +0900

    docs: ログアウトAPIのレスポンス定義を追加

--- app/schemas/http_response_body_user_schema.py
+++ app/schemas/http_response_body_user_schema.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, Field
 
 
+class ApiResponseBodyBase(BaseModel):
+    message: str = Field(default='ユーザー登録が成功しました。',
+                         description='処理メッセージ',
+                         examples=['ユーザー登録が成功しました。'])
+
+
 class SignInApiResponseBody(BaseModel):
     message: str = Field(default='ユーザー登録が成功しました。',
                          description='処理メッセージ',
