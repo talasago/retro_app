ef9e4a8098deb2fa8a646ad3262a08bdf0f0e9c7
talasago <42575556+talasago@users.noreply.github.com>
2023-09-10 17:46:41 +0900

    chore: DB接続情報を環境変数化

--- app/database.py
+++ app/database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import Engine
+import os
+
+# HACK:後で大文字にする
+db_username = os.environ['POSTGRES_USER']
+db_password = os.environ['POSTGRES_PASSWORD']
+db_host = os.environ['POSTGRES_HOST']
+db_name = os.environ['POSTGRES_DATABASE']
 
 # TODO:本番環境の設定を別途検討する必要あり
 # TODO:alembic.iniと共通化した方が良さそう。環境変数とかかなあ
-SQLALCHEMY_DATABASE_URL: str = 'postgresql://postgres:postgres_password@localhost:5432/postgres'
+DATABASE_URL: str = f'postgresql://{db_username}:{db_password}@{db_host}:5432/{db_name}'
 
-engine: Engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine: Engine = create_engine(DATABASE_URL)
 SessionLocal: sessionmaker = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 Base: any = declarative_base()
