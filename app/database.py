5c338deadd77cf8fc0e81d0018c85c9204e05ba4
talasago <42575556+talasago@users.noreply.github.com>
2023-09-22 20:28:37 +0900

    style: 変数名を大文字

--- app/database.py
+++ app/database.py
@@ -3,15 +3,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import Engine
 import os
 
-# HACK:後で大文字にする
-db_username = os.environ['POSTGRES_USER']
-db_password = os.environ['POSTGRES_PASSWORD']
-db_host = os.environ['POSTGRES_HOST']
-db_name = os.environ['POSTGRES_DATABASE']
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+POSTGRES_HOST = os.environ['POSTGRES_HOST']
+POSTGRES_DATABASE = os.environ['POSTGRES_DATABASE']
 
 # TODO:本番環境の設定を別途検討する必要あり
 # TODO:alembic.iniと共通化した方が良さそう。環境変数とかかなあ
-DATABASE_URL: str = f'postgresql://{db_username}:{db_password}@{db_host}:5432/{db_name}'
+DATABASE_URL: str = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DATABASE}'
 
 engine: Engine = create_engine(DATABASE_URL)
 SessionLocal: sessionmaker = sessionmaker(
