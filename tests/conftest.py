d77c8a7373c08dbb1eb4c5ca5caaab82cc19e544
talasago <42575556+talasago@users.noreply.github.com>
2023-09-23 19:45:11 +0900

    test: pytest実行時のテーブル削除方法を変更
    今までSQLAlchemyのモデルクラスをテスト実行時に作っていた。

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,26 +11,17 @@ from sqlalchemy.orm.session import Session  # noqa: E402
 from alembic import command  # noqa: E402
 from alembic.config import Config  # noqa: E402
 from app.database import SessionLocal, Base  # noqa: E402
-from sqlalchemy import create_engine  # noqa: E402
+from sqlalchemy import create_engine, text  # noqa: E402
 from app.repository.user_repository import UserRepository  # noqa: E402
 
 
-# TODO:AutoUseをtrueにしたい
-# TODO:関数名をdb_sessionにしたい
 @pytest.fixture(scope='session')
 def db() -> Session:
-    """データベースセッションのフィクスチャ。TGLを削除→作成→テスト実行→DB接続セッション削除している"""
+    """データベースセッションのフィクスチャ。TBLを削除→作成→テスト実行→DB接続セッション削除としている"""
 
     # NOTE:TBL削除→TBL作成→→テスト実行できるようにすることで、テストデータを毎回削除する手間を減らしている。
-    # ロジックはこちらのHPを参考にしたかった
+    # ロジックはこちらのHPを参考にし
     # https://nikaera.com/archives/pytest-sqlalchemy-alembic/
-    # ホントはDBを消すべきなのだろうけど。`ERROR: cannot drop the currently open database`となって
-    # 削除できない
-    # 現状はTBLの削除で誤魔化している
-    # https://www.postgresql.jp/document/7.3/reference/sql-dropdatabase.htmlを見ると
-    # >対象とするデータベースに接続している場合、このコマンドを実行することができません。とあるので、
-    # 時的に別のデータベースに接続し、テスト用のデータベースをドロップすれば行けるのかもしれない。
-    # 時間かかりそうなので一旦後回し。
 
     # TODO:環境変数とかにしたほうがいいかも
     TEST_SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres_password@localhost:5432/postgres'
@@ -38,10 +29,9 @@ def db() -> Session:
 
     # テーブルの削除と作成
     Base.metadata.drop_all(bind=engine)
-    # テーブルの作り方がマイグレーションと異なるので、列の場所がマイグレとここで違うの良くないな...
-    Base.metadata.create_all(bind=engine)
-
-    # NOTE:これマイグレートしてる意味あるのかな。DB消してないから多分意味ない.
+    with engine.connect() as connection:
+        # マイグレートのバージョン管理しているテーブルも削除することで、マイグレートできる
+        connection.execute(text('DROP TABLE IF EXISTS alembic_version;'))
     migrate()
 
     db = SessionLocal()
