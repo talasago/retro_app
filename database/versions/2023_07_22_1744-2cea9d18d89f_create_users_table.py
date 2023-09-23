30fd9291e22fbf07f62652e71402a01576dec8e0
talasago <42575556+talasago@users.noreply.github.com>
2023-09-23 22:57:31 +0900

    chore: alembicのテーブル定義が間違っていたため修正

--- database/versions/2023_07_22_1744-2cea9d18d89f_create_users_table.py
+++ database/versions/2023_07_22_1744-2cea9d18d89f_create_users_table.py
@@ -19,11 +19,11 @@ depends_on = None
 def upgrade() -> None:
     op.create_table(
         'users',
-        sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-        # TODO:uuidは認証のやり取りで必要と思い追加したが、もし不要な列になったらあとで削除する
-        sa.Column('uuid', sa.UUID(), nullable=False),
-        sa.Column('email', sa.String(), nullable=False),
-        sa.Column('name', sa.String(), nullable=False),
+        sa.Column('id', sa.Integer(), primary_key=True, nullable=False),
+        # sa.Column('id', sa.Integer(), sa.Sequence('user_id_seq'), nullable=False),  # noqa: E501
+        sa.Column('uuid', sa.UUID(), nullable=False, unique=True),
+        sa.Column('email', sa.String(), nullable=False, unique=True),
+        sa.Column('name', sa.String(), nullable=False, unique=True),
         sa.Column('hashed_password', sa.String(), nullable=False),
         sa.Column('created_at', sa.DateTime(), nullable=False),
         sa.Column('updated_at', sa.DateTime(), nullable=False),
