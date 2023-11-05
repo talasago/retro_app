ce6c0dc60d401675affcbefd748bdd0b41232236
talasago <42575556+talasago@users.noreply.github.com>
2023-11-05 10:12:48 +0900

    refactor: tests/配下のファイルをformat実行

--- tests/schemas/test_user_schema.py
+++ tests/schemas/test_user_schema.py
@@ -1,6 +1,7 @@
 import pytest
-from app.schemas.user_schema import UserSchema, UserCreate
 from pydantic import ValidationError
+
+from app.schemas.user_schema import UserCreate, UserSchema
 
 # FIXME:バリデーションのエラーメッセージも確認した方が良い
 # ValidationErrorが返ってきているのはわかるが、なぜそうなったかの確認が足りない
@@ -9,19 +10,19 @@ from pydantic import ValidationError
 class TestUserSchema:
     def test_email_format_valid(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
         }
 
         user_params = UserSchema(**user_data)
 
-        assert user_params.name == 'John Doe'
-        assert user_params.email == 'johndoe1@example.com'
+        assert user_params.name == "John Doe"
+        assert user_params.email == "johndoe1@example.com"
 
     def test_email_format_invalid(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'invalid_email',
+            "name": "John Doe",
+            "email": "invalid_email",
         }
 
         with pytest.raises(ValidationError):
@@ -31,7 +32,7 @@ class TestUserSchema:
 
     def test_email_null(self):
         user_data: dict = {
-            'name': 'email null',
+            "name": "email null",
         }
 
         with pytest.raises(ValidationError):
@@ -39,10 +40,7 @@ class TestUserSchema:
 
             assert user_params is None
 
-        user_data: dict = {
-            'name': 'email null',
-            'email': None
-        }
+        user_data: dict = {"name": "email null", "email": None}
 
         with pytest.raises(ValidationError):
             user_params = UserSchema(**user_data)
@@ -51,8 +49,8 @@ class TestUserSchema:
 
     def test_name_invalid_max_len(self):
         user_data: dict = {
-            'name': 'あ' * 51,
-            'email': 'johndoe1@example.com',
+            "name": "あ" * 51,
+            "email": "johndoe1@example.com",
         }
 
         with pytest.raises(ValidationError):
@@ -62,19 +60,19 @@ class TestUserSchema:
 
     def test_name_valid_max_len(self):
         user_data: dict = {
-            'name': '  ' + 'あ' * 24 + ' 　' + 'あ' * 24 + '　　',
-            'email': 'johndoe1@example.com',
+            "name": "  " + "あ" * 24 + " 　" + "あ" * 24 + "　　",
+            "email": "johndoe1@example.com",
         }
 
         user_params = UserSchema(**user_data)
 
-        assert user_params.name == 'あ' * 24 + ' 　' + 'あ' * 24
-        assert user_params.email == 'johndoe1@example.com'
+        assert user_params.name == "あ" * 24 + " 　" + "あ" * 24
+        assert user_params.email == "johndoe1@example.com"
 
     def test_name_null(self):
         user_data: dict = {
-           'name': None,
-           'email': 'johndoe1@example.com',
+            "name": None,
+            "email": "johndoe1@example.com",
         }
 
         with pytest.raises(ValidationError):
@@ -83,7 +81,7 @@ class TestUserSchema:
             assert user_params is None
 
         user_data: dict = {
-           'email': 'johndoe1@example.com',
+            "email": "johndoe1@example.com",
         }
 
         with pytest.raises(ValidationError):
@@ -95,8 +93,8 @@ class TestUserSchema:
 class TestUserCreate:
     def test_password_null(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
         }
 
         with pytest.raises(ValidationError):
@@ -105,9 +103,9 @@ class TestUserCreate:
             assert user_params is None
 
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': None
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": None,
         }
 
         with pytest.raises(ValidationError):
@@ -117,9 +115,9 @@ class TestUserCreate:
 
     def test_password_invalid_length(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': 'a' * 7
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": "a" * 7,
         }
 
         with pytest.raises(ValidationError):
@@ -128,9 +126,9 @@ class TestUserCreate:
             assert user_params is None
 
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': 'a' * 51
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": "a" * 51,
         }
 
         with pytest.raises(ValidationError):
@@ -140,42 +138,42 @@ class TestUserCreate:
 
     def test_password_valid_length(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': 'a' * 8
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": "a" * 8,
         }
 
         user_params = UserSchema(**user_data)
 
-        assert user_params.name == 'John Doe'
-        assert user_params.email == 'johndoe1@example.com'
+        assert user_params.name == "John Doe"
+        assert user_params.email == "johndoe1@example.com"
 
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': 'a' * 50
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": "a" * 50,
         }
         user_params = UserSchema(**user_data)
 
-        assert user_params.name == 'John Doe'
-        assert user_params.email == 'johndoe1@example.com'
+        assert user_params.name == "John Doe"
+        assert user_params.email == "johndoe1@example.com"
 
     def test_password_valid_format(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': '1sA!?_+*\'"`#$%&-^\\@;:,./=~|[](){}<>'
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": "1sA!?_+*'\"`#$%&-^\\@;:,./=~|[](){}<>",
         }
         user_params = UserSchema(**user_data)
 
-        assert user_params.name == 'John Doe'
-        assert user_params.email == 'johndoe1@example.com'
+        assert user_params.name == "John Doe"
+        assert user_params.email == "johndoe1@example.com"
 
     def test_password_invalid_format(self):
         user_data: dict = {
-            'name': 'John Doe',
-            'email': 'johndoe1@example.com',
-            'password': 'ＰＡＳＳＷＯＲＤ'
+            "name": "John Doe",
+            "email": "johndoe1@example.com",
+            "password": "ＰＡＳＳＷＯＲＤ",
         }
 
         with pytest.raises(ValidationError):
--- tests/test_helpers/token.py
+++ tests/test_helpers/token.py
@@ -1,20 +1,21 @@
-from uuid import uuid4
-from jose import jwt
 from datetime import datetime, timedelta
+from uuid import uuid4
+
+from jose import jwt
+
 from app.schemas.token_schema import TokenPayload, TokenType
 
 
-def generate_test_token(token_type: TokenType,
-                        user_uuid=uuid4(),
-                        exp=datetime.utcnow() + timedelta(minutes=100),
-                        ) -> str:
+def generate_test_token(
+    token_type: TokenType,
+    user_uuid=uuid4(),
+    exp=datetime.utcnow() + timedelta(minutes=100),
+) -> str:
     token_payload = TokenPayload(
-        token_type=token_type,
-        exp=exp,
-        uid=str(user_uuid),
-        jti=str(uuid4())
+        token_type=token_type, exp=exp, uid=str(user_uuid), jti=str(uuid4())
     )
-    token: str = jwt.encode(claims=token_payload.model_dump(),
-                            key='secret_key', algorithm='HS256')
+    token: str = jwt.encode(
+        claims=token_payload.model_dump(), key="secret_key", algorithm="HS256"
+    )
 
     return token
