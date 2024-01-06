f3584f83c1aa3542213c75deb9076a663b6ffc0b
talasago <42575556+talasago@users.noreply.github.com>
2024-01-06 22:19:52 +0900

    ちょっと修正

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-// 後でmain.tsxに変更する
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 import './resets.css';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
