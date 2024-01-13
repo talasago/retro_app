19628842aa9b67d2f70c7dcc3676851417aea079
talasago <42575556+talasago@users.noreply.github.com>
2024-01-13 23:10:18 +0900

    formの基本処理追加

--- client/src/components/container/RegistrationForm.tsx
+++ client/src/components/container/RegistrationForm.tsx
@@ -1,5 +1,8 @@
 import type { FC } from 'react';
 import { Box, Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
+
 // これ実はpresentasion componentだったりするかも
 interface FormData {
   email: string;
@@ -9,14 +12,30 @@ interface FormData {
 
 const RegistrationForm: FC = () => {
   // TODO:アプリ名を入れる
+
+  // MEMO: formStateのisSubmittingとか使えば二重送信防止とかできるかも
+  const { register, handleSubmit } = useForm<FormData>({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+    shouldFocusError: true,
+  });
+  // TODO: ほんとは戻り値3つ目を使ってresetとかclearErrorsの実装した方が良さげ
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    // TODO: APIを叩くようにする
+    console.log(data);
+  };
+
   return (
     <Box padding={3}>
-      <form action="/hoge">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Box display="flex" flexDirection="column" sx={{ gap: 2 }}>
-          <TextField label="Email" />
-          <TextField label="Name" />
-          <TextField label="Password" />
-          <Button variant="contained">登録する</Button>
+          <TextField label="Email" {...register('email')} />
+          <TextField label="Name" {...register('name')} />
+          <TextField label="Password" {...register('password')} />
+          <Button variant="contained" type="submit">
+            登録する
+          </Button>
         </Box>
       </form>
     </Box>
