34f5aec8be95d1ccb875e1fcfc8ab3f35384df61
talasago <42575556+talasago@users.noreply.github.com>
2024-01-14 20:51:26 +0900

    不要ファイル削除

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,8 @@
-import { memo } from 'react';
 import type { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
 
-import { Frame1 } from './components/Frame1/Frame1';
-import resets from './components/_resets.module.css';
 import RegistrationForm from './features/SignUp/components/container/RegistrationForm';
-import classes from './App.module.css';
-interface Props {
-  className?: string;
-}
 
-// 多分、classNameがそんなに変わらないのでmemo化してるのだと思われる。このprops辞退削除しても良さそうかも
-// export const App: FC<Props> = memo(function App(props = {}) {
-//  return (
-//    <div className={`${resets.storybrainResets} ${classes.root}`}>
-//      <Frame1 />
-//    </div>
-//  );
-// });
 const appTitle = import.meta.env.VITE_APP_TITLE;
 
 const App: FC = () => (
@@ -28,4 +13,5 @@ const App: FC = () => (
     <RegistrationForm />
   </div>
 );
+
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
-import './resets.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
