fb394a21b41ee64095d91f6876715f69b5cdf91d
talasago <42575556+talasago@users.noreply.github.com>
2024-01-14 12:58:23 +0900

    feat: yupを使用した基本的なバリデーションを追加

--- client/src/features/SignUp/components/container/RegistrationForm.tsx
+++ client/src/features/SignUp/components/container/RegistrationForm.tsx
@@ -1,27 +1,35 @@
+// これ実はpresentasion componentだったりするかも
 import type { FC } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControl,
+  TextField,
+  FormHelperText,
+} from '@mui/material';
 import { useForm } from 'react-hook-form';
 import type { SubmitHandler } from 'react-hook-form';
-
-// これ実はpresentasion componentだったりするかも
-interface FormData {
-  email: string;
-  name: string;
-  password: string;
-}
+import { yupResolver } from '@hookform/resolvers/yup';
+import { registrationFormSchema } from '../schemas/registrationFormSchema';
+import type { RegistrationFormSchema } from '../schemas/registrationFormSchema';
 
 const RegistrationForm: FC = () => {
   // TODO:アプリ名を入れる
 
   // MEMO: formStateのisSubmittingとか使えば二重送信防止とかできるかも
-  const { register, handleSubmit } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegistrationFormSchema>({
     mode: 'onSubmit',
-    reValidateMode: 'onSubmit',
+    reValidateMode: 'onChange',
     shouldFocusError: true,
+    resolver: yupResolver(registrationFormSchema),
   });
-  // TODO: ほんとは戻り値3つ目を使ってresetとかclearErrorsの実装した方が良さげ
+  // TODO: ほんとは戻り値を使ってresetとかclearErrorsの実装した方が良さげ
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
+  const onSubmit: SubmitHandler<RegistrationFormSchema> = (data) => {
     // TODO: APIを叩くようにする
     console.log(data);
   };
@@ -30,9 +38,18 @@ const RegistrationForm: FC = () => {
     <Box padding={3}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Box display="flex" flexDirection="column" sx={{ gap: 2 }}>
-          <TextField label="Email" {...register('email')} />
-          <TextField label="Name" {...register('name')} />
-          <TextField label="Password" {...register('password')} />
+          <FormControl error={errors.email !== undefined}>
+            <TextField label="メールアドレス" {...register('email')} />
+            <FormHelperText>{errors.email?.message}</FormHelperText>
+          </FormControl>
+          <FormControl error={errors.name !== undefined}>
+            <TextField label="名前" {...register('name')} />
+            <FormHelperText>{errors.name?.message}</FormHelperText>
+          </FormControl>
+          <FormControl error={errors.password !== undefined}>
+            <TextField label="パスワード" {...register('password')} />
+            <FormHelperText>{errors.password?.message}</FormHelperText>
+          </FormControl>
           <Button variant="contained" type="submit">
             登録する
           </Button>
