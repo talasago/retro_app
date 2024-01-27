2a08ab970391a9ad22edcee8a05d97253b89b942
talasago <42575556+talasago@users.noreply.github.com>
2024-01-27 18:20:16 +0900

    feat: SSMからDBパスワードを取得する

--- app/database.py
+++ app/database.py
@@ -1,35 +1,17 @@
 import os
 
-import boto3
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.helpers.utils import is_local_execution, load_env_for_local
 
+from .db_password import DbPasswordFactory
+
 if is_local_execution():
     load_env_for_local()
 
-
-def get_db_password() -> str:
-    if is_local_execution():
-        return os.environ["POSTGRES_PASSWORD"]
-
-    # 平文で環境変数に設定したくないのでparameter storeから取得する
-    ssm = boto3.client("ssm", region_name="ap-northeast-1")
-    response = ssm.get_parameter(Name="postgres_database", WithDecryption=True)
-    return response["Parameter"]["Value"]
-    # ssmの操作とかを別ファイルに移動して責務を分解してももいいかもしれないが、他にssm使う予定もないこと、
-    # このファイル単体でテストすることは無いので移動してない。
-
-    # 将来、boto3使わずともlambda拡張でparameterstoreの値を取得できるかも。
-    # 現在はARMサポートされてないらしい。
-    # Lambda拡張にすることでコストが削減できるかもしれないのでメモ
-    # https://docs.aws.amazon.com/systems-manager/latest/userguide/ps-integration-lambda-extensions.html
-    # https://dev.classmethod.jp/articles/lambda-get-paramater/
-
-
-POSTGRES_PASSWORD = get_db_password()
+POSTGRES_PASSWORD = DbPasswordFactory.create_db_password().get_db_password()
 POSTGRES_USER = os.environ["POSTGRES_USER"]
 POSTGRES_HOST = os.environ["POSTGRES_HOST"]
 POSTGRES_DATABASE = os.environ["POSTGRES_DATABASE"]
