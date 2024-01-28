59cc58be0fc54b071db4d4ca059f8f526fd7d6ab
talasago <42575556+talasago@users.noreply.github.com>
2024-01-28 13:23:47 +0900

    ssmから取得を一旦やめる

--- app/database.py
+++ app/database.py
@@ -6,21 +6,20 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.helpers.utils import is_local_execution, load_env_for_local
 
-from .db_password import DbPasswordFactory
+# from .db_password import DbPasswordFactory
 
 if is_local_execution():
     load_env_for_local()
 
-POSTGRES_PASSWORD = DbPasswordFactory.create().get_db_password()
+# POSTGRES_PASSWORD = DbPasswordFactory.create().get_db_password()
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
 POSTGRES_USER = os.environ["POSTGRES_USER"]
 POSTGRES_HOST = os.environ["POSTGRES_HOST"]
 POSTGRES_DATABASE = os.environ["POSTGRES_DATABASE"]
 
 # TODO:本番環境の設定を別途検討する必要あり
 # TODO:alembic.iniと共通化した方が良さそう。環境変数とかかなあ
-DATABASE_URL: str = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DATABASE}"
-)
+DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DATABASE}"
 
 engine: Engine = create_engine(DATABASE_URL)
 SessionLocal: sessionmaker = sessionmaker(
