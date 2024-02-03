5b40aabfea3779476d7cdb4a2c2803d77c508b5d
talasago <42575556+talasago@users.noreply.github.com>
2024-02-03 15:36:23 +0900

    型ヒントの修正

--- tests/test_helpers/functions/conftest_user_api.py
+++ tests/test_helpers/functions/conftest_user_api.py
@@ -22,13 +22,16 @@ def add_user_api():
 
     return _method
 
+# responseが200系であることの検証を追加する
 
 @pytest.fixture(scope="session")
 def login_api():
     # MEMO: is_return_responseは削除してもいいんじゃないか感
     # responseが欲しいのか、それともtokenが欲しいかで関心ごとが違うのか
+    # MEMO: tupleで返すときは何を返すかの型ヒントが良いかもしれない。TokenType
+    # FIXME:is_return_responseの引数名とデフォルト引数を考える。他のapi()に合わせて、通常はresponseを返すようにする
     def _method(login_param: dict, is_return_response=False) -> Response | tuple:
-        response: "Response" = client_user.post(
+        response: Response = client_user.post(
             "/token",
             headers={
                 "accept": "application/json",
@@ -48,8 +51,8 @@ def login_api():
 
 @pytest.fixture(scope="session")
 def refresh_token_api():
-    def _method(refresh_token: str) -> "Response":
-        response: "Response" = client_user.post(
+    def _method(refresh_token: str) -> Response:
+        response: Response = client_user.post(
             "/refresh_token",
             headers={
                 "accept": "application/json",
@@ -63,7 +66,7 @@ def refresh_token_api():
 
 @pytest.fixture(scope="session")
 def logout_api():
-    def _method(access_token: str) -> "Response":
+    def _method(access_token: str) -> Response:
         response = client_user.post(
             "/api/v1/logout",
             headers={
