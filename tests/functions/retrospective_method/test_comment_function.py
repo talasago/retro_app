ac6ab683acac8694505d5bf0f15571c5d73e0457
talasago <42575556+talasago@users.noreply.github.com>
2024-02-04 15:59:03 +0900

    コメント削除

--- tests/functions/retrospective_method/test_comment_function.py
+++ tests/functions/retrospective_method/test_comment_function.py
@@ -3,11 +3,9 @@ import pytest
 from tests.test_helpers.functions.cors import assert_cors_headers
 
 
-# TODO : ユーザ作成~ログイン周りの共通関数
 @pytest.mark.usefixtures("db")
 class TestCommentFunction:
     class TestAddComment:
-        # TODO:TestLogin用のユーザーを作りたいなあ。毎回テストの中で作るのをやめたい。fixture使えばいいのか
         class TestValidParam:
             def test_return_201(
                 self, tokens_of_logged_in_api_common_user, add_comment_api
