59970af1aafcc8fae9841b455fdb774aef9a3aef
talasago <42575556+talasago@users.noreply.github.com>
2024-04-29 11:45:06 +0900

    feat: ログアウト時、ログイン必須なAPIを叩くときの処理を作成

--- client/src/components/container/Header.tsx
+++ client/src/components/container/Header.tsx
@@ -1,8 +1,20 @@
 import type { FC } from 'react';
+import {
+  type ActionCreatorWithPayload,
+  type ActionCreatorWithoutPayload,
+} from '@reduxjs/toolkit';
 import axios from 'axios';
-import { LOGOUT_URL } from 'domains/internal/constants/apiUrls';
+import type { AxiosResponse, Method } from 'axios';
+import {
+  LOGOUT_URL,
+  REFRESH_TOKEN_URL,
+} from 'domains/internal/constants/apiUrls';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link as RouterLink } from 'react-router-dom';
+import {
+  NavigateFunction,
+  Link as RouterLink,
+  useNavigate,
+} from 'react-router-dom';
 import { ROUTES_LISTS } from 'routes';
 import { alertSlice } from 'stores/alert';
 import { authSlice } from 'stores/auth';
@@ -17,37 +29,44 @@ const Header: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { setAlert } = alertSlice.actions;
   const auth: AuthState = useSelector((state: RootState) => state.auth);
-  const { resetToken } = authSlice.actions;
+  const { resetToken, setToken } = authSlice.actions;
+  const navigate = useNavigate();
 
   const handleLogout = async (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault(); // リンクをクリックするとページの最上部にスクロールしないようにする
 
-    try {
-      const response = await axios.post(LOGOUT_URL, '', {
-        headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${auth.accessToken}`,
-        },
-      });
+    const [response, error] = await callProtectedApi(
+      LOGOUT_URL,
+      'POST',
+      '',
+      auth,
+      dispatch,
+      setToken,
+      resetToken,
+      navigate,
+    );
+
+    if (error) {
       dispatch(
         setAlert({
           open: true,
-          message: 'ログアウトが成功したで',
-          severity: 'success',
-        }),
-      );
-      dispatch(resetToken());
-      console.log('Response:', response.data);
-    } catch (error) {
-      dispatch(
-        setAlert({
-          open: true,
-          message: 'ログアウトAPIエラーになってるで',
+          message: error.message,
           severity: 'error',
         }),
       );
       console.error('Error:', error);
+
+      return;
     }
+    dispatch(
+      setAlert({
+        open: true,
+        message: 'ログアウトが成功したで',
+        severity: 'success',
+      }),
+    );
+    dispatch(resetToken());
+    console.log('Response:', response?.data);
   };
 
   return (
@@ -79,7 +98,7 @@ const Header: FC = () => {
             color="inherit"
             onClick={handleLogout}
             sx={{ margin: '10px' }}
-            hidden={!auth.isLogined}
+            // hidden={!auth.isLogined}
           >
             ログアウト
           </Link>
@@ -90,3 +109,129 @@ const Header: FC = () => {
 };
 
 export default Header;
+
+// ほんとはauthとかdispachとかsetTokenとかresetTokenを受け取りたくない。呼び出し先でやってほしい気持ち。
+const callProtectedApi = async (
+  url: string,
+  method: Method,
+  data = '',
+  auth: AuthState,
+  dispatch: AppDispatch,
+  setToken: ActionCreatorWithPayload<AuthState, 'auth/setToken'>,
+  resetToken: ActionCreatorWithoutPayload<'auth/resetToken'>,
+  navigate: NavigateFunction,
+): Promise<[AxiosResponse<any, any> | null, Error | null]> => {
+  // await/catchの方が良いかもなあ。それかthenとかcatchの方が居良いかも
+  try {
+    const response = await axios({
+      method,
+      url,
+      data,
+      headers: apiHeaders(auth.accessToken),
+    });
+
+    return [response, null];
+  } catch (error) {
+    if (!isTokenExpired(error)) {
+      return [
+        null,
+        new Error(
+          'エラーが発生しました。時間をおいて再実行してください。',
+          error as Error,
+        ),
+      ];
+    }
+
+    let updatedAccessToken: string = '';
+    try {
+      updatedAccessToken = await updateTokenUseRefreshToken(
+        auth.refreshToken,
+        dispatch,
+        setToken,
+      );
+    } catch (error) {
+      if (!isTokenExpired(error)) {
+        return [
+          null,
+          new Error(
+            'エラーが発生しました。時間をおいて再実行してください。',
+            error as Error,
+          ),
+        ];
+      }
+
+      dispatch(resetToken());
+      navigate('/login');
+
+      return [
+        null,
+        new Error(
+          'ログイン有効期間を過ぎています。再度ログインしてください。',
+          error as Error,
+        ),
+      ];
+    }
+
+    try {
+      const response = await axios({
+        method,
+        url,
+        data,
+        headers: apiHeaders(updatedAccessToken),
+      });
+
+      return [response, null];
+    } catch (error) {
+      return [
+        null,
+        new Error(
+          'エラーが発生しました。時間をおいて再実行してください。',
+          error as Error,
+        ),
+      ];
+    }
+  }
+};
+
+const isTokenExpired = (error: unknown): boolean => {
+  return (
+    axios.isAxiosError(error) &&
+    error.response?.status === 401 &&
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    error.response?.data?.detail ===
+      'ログイン有効期間を過ぎています。再度ログインしてください。'
+  );
+};
+
+const apiHeaders = (token: string) => {
+  return {
+    accept: 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+};
+
+const updateTokenUseRefreshToken = async (
+  refreshToken: string,
+  dispatch: AppDispatch,
+  setToken: ActionCreatorWithPayload<AuthState, 'auth/setToken'>,
+): Promise<string> => {
+  let updatedAccessToken = '';
+
+  const responseRefToken = await axios.post(REFRESH_TOKEN_URL, '', {
+    headers: apiHeaders(refreshToken),
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  updatedAccessToken = responseRefToken.data.access_token;
+
+  dispatch(
+    setToken({
+      isLogined: true,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      accessToken: responseRefToken.data.access_token,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      refreshToken: responseRefToken.data.refresh_token,
+    }),
+  );
+
+  return updatedAccessToken;
+};
