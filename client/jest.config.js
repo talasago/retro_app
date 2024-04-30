11a063f97b83756ae87df4253befaf57df0fa937
talasago <42575556+talasago@users.noreply.github.com>
2024-04-30 11:10:24 +0900

    chore: install test lib and change setting to run jest

---
+++ client/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^domains/(.*)$': '<rootDir>/src/domains/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  setupFiles: ['<rootDir>/jestSetup.ts'],
+};
