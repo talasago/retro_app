8b6d92acab9cceecb72b328fc7493e33cfa7b8f5
talasago <42575556+talasago@users.noreply.github.com>
2024-09-07 19:20:33 +0900

    feat: Use useForm in LoginModal

--- client/src/features/Login/components/container/LoginModal.tsx
+++ client/src/features/Login/components/container/LoginModal.tsx
@@ -1,25 +1,75 @@
 // TODO:各パラメータの調整確認
 // TODO:デザインとの差異確認
-// TODO:モーダル閉じない時があるもんだ
+// TODO:モーダル閉じない時があるもんだい
+// TODO:ログインフォームの削除
+// TODO:ログインフォームの内容からロジックを移植
+// presenterとcontainerの分離
 
 import type { FC } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import {
   Box,
   Button,
   Container,
   Paper,
+  FormControl,
   TextField,
   Avatar,
   Modal,
+  FormHelperText,
+  CircularProgress,
 } from '@mui/material';
+import axios from 'axios';
+import { LOGIN_URL } from 'domains/internal/constants/apiUrls';
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { alertSlice } from 'stores/alert';
+import type { AppDispatch } from 'stores/store';
 import LockIcon from '@mui/icons-material/Lock';
+import { AuthToken } from 'domains/AuthToken';
+import { loginFormSchema } from '../schemas/loginFormSchema';
+import type { LoginFormSchema } from '../schemas/loginFormSchema';
 
 interface LoginModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const loginUser = async (requestBody: LoginFormSchema) => {
+  const params = new URLSearchParams();
+  params.append('username', requestBody.name);
+  params.append('password', requestBody.password);
+
+  return await axios.post(LOGIN_URL, params, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+};
+
+// TODO: ヘッダーとこれはuseMemoを使ってもいいかも
 const LoginModal: FC<LoginModalProps> = ({ isOpen, onClose }): JSX.Element => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { setAlert } = alertSlice.actions;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormSchema>({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    shouldFocusError: true,
+    resolver: yupResolver(loginFormSchema),
+  });
+  // MEMO: ほんとは戻り値を使ってresetとかclearErrorsの実装した方が良さげ
+
+  const onSubmit: SubmitHandler<LoginFormSchema> = async (loginFormData) => {
+    console.log(loginFormData);
+    // TODO:ログインロジックを実装
+  };
+
   return (
     <div>
       <Modal
@@ -45,39 +95,59 @@ const LoginModal: FC<LoginModalProps> = ({ isOpen, onClose }): JSX.Element => {
           }}
         >
           <Paper elevation={3} sx={{ width: 400, padding: 4, borderRadius: 2 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mb: 4 }}>
-              <Avatar sx={{ bgcolor: 'grey.800', width: 90, height: 90 }}>
-                <LockIcon sx={{ fontSize: 50 }} />
-              </Avatar>
-            </Box>
-            <TextField
-              fullWidth
-              variant="filled"
-              label="ユーザーネーム"
-              sx={{ mb: 2, bgcolor: 'grey.200', borderRadius: 1 }}
-            />
-            <TextField
-              fullWidth
-              variant="filled"
-              label="パスワード"
-              type="password"
-              sx={{ mb: 2, bgcolor: 'grey.200', borderRadius: 1 }}
-            />
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              sx={{ mb: 2, bgcolor: 'grey.800', height: 50 }}
-            >
-              ログイン
-            </Button>
-            <Button
-              fullWidth
-              variant="outlined"
-              sx={{ height: 50, borderColor: 'grey.600', color: 'grey.600' }}
-            >
-              ユーザー登録はこちら
-            </Button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', mb: 4 }}>
+                <Avatar sx={{ bgcolor: 'grey.800', width: 90, height: 90 }}>
+                  <LockIcon sx={{ fontSize: 50 }} />
+                </Avatar>
+              </Box>
+              <FormControl fullWidth error={errors.name !== undefined}>
+                <TextField
+                  {...register('name')}
+                  fullWidth
+                  variant="filled"
+                  label="ユーザー名"
+                  sx={{
+                    mb: 2,
+                    bgcolor: 'grey.200',
+                    borderRadius: 1,
+                  }}
+                />
+                <FormHelperText>{errors.name?.message}</FormHelperText>
+              </FormControl>
+              <FormControl fullWidth error={errors.password !== undefined}>
+                <TextField
+                  {...register('password')}
+                  variant="filled"
+                  label="パスワード"
+                  type="password"
+                  sx={{
+                    mb: 2,
+                    bgcolor: 'grey.200',
+                    borderRadius: 1,
+                    width: '100%',
+                  }}
+                />
+                <FormHelperText>{errors.password?.message}</FormHelperText>
+              </FormControl>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ mb: 2, bgcolor: 'grey.800', height: 50 }}
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? <CircularProgress size={24} /> : 'ログインする'}
+              </Button>
+              <Button
+                fullWidth
+                variant="outlined"
+                sx={{ height: 50, borderColor: 'grey.600', color: 'grey.600' }}
+              >
+                ユーザー登録はこちら
+              </Button>
+            </form>
           </Paper>
         </Container>
       </Modal>
