f41202cccf8d7fafc6b5aea608f3cbb87b00040c
talasago <42575556+talasago@users.noreply.github.com>
2024-09-12 22:15:36 +0900

    Feat(comment): use CommentSchema

--- app/functions/retrospective_method/comment.py
+++ app/functions/retrospective_method/comment.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from fastapi import Depends, FastAPI, status
 from fastapi.responses import JSONResponse
 from mangum import Mangum
+from pydantic import ValidationError
 
 from app.functions.dependencies import (
     get_comment_repo,
@@ -16,17 +17,41 @@ from app.models.user_model import UserModel
 from app.schemas.http_response_body_user_schema import (
     ApiResponseBodyBase,
 )
-from app.schemas.retrospective_method.comment_schema import CommentCreate
-
-# from app.schemas.user_schema import UserCreate
+from app.schemas.retrospective_method.comment_schema import CommentCreate, CommentSchema
+from app.schemas.translations.i18n_translate_wrapper import I18nTranslateWrapper
 
 # 型アノテーションだけのimport。これで本番実行時はインポートされなくなり、処理速度が早くなるはず
 if TYPE_CHECKING:
+    from fastapi import Request
+    from pydantic_core import ErrorDetails
+
     from app.repository.retrospective_method.comment_repository import CommentRepository
 
 
 app = FastAPI()
 add_cors_middleware(app)
+
+
+# TODO: 共通化
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(
+    _: "Request", exc: ValidationError
+) -> JSONResponse:
+    errors: list[ErrorDetails] = exc.errors()
+
+    for error in errors:
+        if "ctx" in error and isinstance(
+            error.get("ctx", {}).get("error", ""), ValueError
+        ):
+            # pydenticのカスタムバリデーションを使ったとき、
+            # ctx.errorに"ValueError(hogehoge)"となるとJSONに変換できないため、strに変換する
+            error["ctx"]["error"] = str(error["ctx"]["error"])
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": I18nTranslateWrapper.trans(errors)},  # type: ignore
+        #  type(exc.errors()) => listとなっていることを確認している
+    )
 
 
 # TODO : 後でやる openAPI response_model=ApiResponseBodyBase
@@ -42,13 +67,12 @@ def add_comment(
 ):
     """コメント登録のエンドポイント。"""
 
-    comment: CommentModel = CommentModel(
+    comment = CommentSchema(
         retrospective_method_id=retrospective_method_id,
         user_id=current_user.id,
         comment=comment_params.comment,
     )
-    # TODO:重複エラーの時、4xx系を返すようにする
-    comment_repo.save(comment)
+    comment_repo.save(CommentModel(**comment.model_dump()))
 
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
--- tests/functions/retrospective_method/test_comment_function.py
+++ tests/functions/retrospective_method/test_comment_function.py
@@ -2,6 +2,14 @@ import pytest
 
 from tests.test_helpers.functions.cors import assert_cors_headers
 
+# functionで追加するテスト
+# - 日本語になっているか？
+# - カスタムバリデーションの場合とそうじゃない時
+# キーが無い時（コメントが無い時）
+# createSchemaでエラーの時
+# Noneの時
+# それぞれでWhenで分ける
+
 
 @pytest.mark.usefixtures("db")
 class TestCommentFunction:
@@ -29,3 +37,18 @@ class TestCommentFunction:
                 add_comment_api,
             ):
                 call_api_with_invalid_access_token_assert_401(add_comment_api)
+
+            def test_return_422_when_comment_is_empty(
+                self, tokens_of_logged_in_api_common_user, add_comment_api
+            ):
+                comment_data: dict = {
+                    "comment": "",
+                }
+
+                response = add_comment_api(
+                    comment_data=comment_data,
+                    access_token=tokens_of_logged_in_api_common_user[0],
+                    is_assert_response_code_2xx=False,
+                )
+                assert response.status_code == 422
+                assert response.json()["detail"][0]["msg"] == "必須項目です。"
