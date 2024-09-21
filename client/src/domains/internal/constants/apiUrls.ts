936926644d1eb27982e2e80163bc2ca72219782e
talasago <42575556+talasago@users.noreply.github.com>
2024-09-21 17:06:19 +0900

    refactor: update API URL prefixes for consistency

--- client/src/domains/internal/constants/apiUrls.ts
+++ client/src/domains/internal/constants/apiUrls.ts
@@ -1,7 +1,7 @@
 import { getEnvVar } from 'utils/getEnvVar'; // ユーティリティ関数のパスに置き換えてください
 
-const prefix = getEnvVar('VITE_BACKEND_API_URL');
-export const SIGN_UP_URL = `${prefix}/api/v1/sign_up`;
+const prefix = getEnvVar('VITE_BACKEND_API_URL') + '/api/v1';
+export const SIGN_UP_URL = `${prefix}/sign_up`;
 export const LOGIN_URL = `${prefix}/token`;
-export const LOGOUT_URL = `${prefix}/api/v1/logout`;
+export const LOGOUT_URL = `${prefix}/logout`;
 export const REFRESH_TOKEN_URL = `${prefix}/refresh_token`;
