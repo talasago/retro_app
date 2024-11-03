b45404c988a0d98d9f65734046cae6a4eb0900c4
talasago <42575556+talasago@users.noreply.github.com>
2024-11-03 23:19:19 +0900

    feat: add comment_service.py

---
+++ app/services/retrospective_method/comment_service.py
@@ -0,0 +1,60 @@
+import json
+from typing import TYPE_CHECKING, Final
+
+if TYPE_CHECKING:
+    from mypy_boto3_stepfunctions import SFNClient
+    from mypy_boto3_stepfunctions.type_defs import (
+        DescribeExecutionOutputTypeDef,
+        StartExecutionOutputTypeDef,
+    )
+
+    from app.schemas.retrospective_method.comment_schema import CommentSchema
+
+class CommentService:
+    STATE_STATUS_CHECK_MAX_RETRY_TIMES: Final[int] = 15
+
+    def __init__(self, sfn_client: "SFNClient", state_machine_arn: str):
+        self.sfn_client = sfn_client
+        self.state_machine_arn = state_machine_arn
+
+    def add_comment_from_api(self, comment: "CommentSchema"):
+        # FIXME: 複雑化しているので、private関数に切り出したい
+        # 1. ステートマシンを起動
+        # 2. 特定の条件に合致してたらlineを呼び出す
+
+        start_execution_res: StartExecutionOutputTypeDef = (
+            self.sfn_client.start_execution(
+                stateMachineArn=self.state_machine_arn,
+                input=comment.model_dump_json(),
+            )
+        )
+        print("started execution")
+
+        for _ in range(self.STATE_STATUS_CHECK_MAX_RETRY_TIMES):
+            describe_execution_res: "DescribeExecutionOutputTypeDef" = (
+                self.sfn_client.describe_execution(
+                    executionArn=start_execution_res["executionArn"]
+                )
+            )
+            state_status = describe_execution_res["status"]
+            print(f"status: {state_status}")
+
+            if state_status in ["SUCCEEDED"]:
+                output: dict = json.loads(describe_execution_res["output"])
+                print(f"output: {output}")
+                payload = output.get("Payload")
+
+                if payload:
+                    print(f"payload: {payload}")
+                    # lineを呼び出すための関数を呼び出す
+
+                break
+            elif state_status in ["FAILED", "TIMED_OUT", "ABORTED"]:
+                # TODO: エラー処理
+                print("error")
+                break
+        else:
+            # TODO: エラー処理
+            print("Max retries reached")
+
+        print("execution finished")
