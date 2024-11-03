a87e0b18c9bf69efc76ad7f2112c3895a5f05bd8
talasago <42575556+talasago@users.noreply.github.com>
2024-11-03 23:19:19 +0900

    feat: add comment_service.py

---
+++ tests/services/retrospective_method/test_comment_service.py
@@ -0,0 +1,56 @@
+# test_sut.py
+import json
+
+import boto3
+import pytest
+from moto import mock_aws
+
+from app.schemas.retrospective_method.comment_schema import CommentSchema
+from app.services.retrospective_method.comment_service import CommentService
+
+
+class TestCommentService:
+    @pytest.fixture
+    def mock_sfn_client(self):
+        with mock_aws():
+            client = boto3.client("stepfunctions", region_name="ap-northeast-1")
+
+            state_machine_definition = {
+                "StartAt": "AddCommentState",
+                "States": {
+                    "AddCommentState": {
+                        "Type": "Task",
+                        "Resource": "arn:aws:lambda:ap-northeast-1:000000000000:function:add_comment_function",
+                        "End": True,
+                    }
+                },
+            }
+
+            response = client.create_state_machine(
+                name="AddCommentStateMachine",
+                definition=json.dumps(state_machine_definition),
+                roleArn="arn:aws:iam::000000000000:role/StateMachineRole",
+            )
+
+            yield client, response["stateMachineArn"]
+
+    @pytest.fixture
+    def sut(self, mock_sfn_client):
+        client, state_machine_arn = mock_sfn_client
+        return CommentService(client, state_machine_arn)
+
+    class TestAddCommentFromApi:
+        # 仮のテスト
+        def test_add_comment_from_api(self, sut):
+            comment = CommentSchema(
+                retrospective_method_id=1, user_id=1, comment="Test comment"
+            )
+
+            sut.add_comment_from_api(comment)
+
+            # TODO:lineのためのメソッドを呼び出していることをテストしたいかも
+            executions = sut.sfn_client.list_executions(
+                stateMachineArn=sut.state_machine_arn
+            )
+            assert len(executions["executions"]) == 1
+            assert executions["executions"][0]["status"] == "RUNNING" # 仮のテスト
