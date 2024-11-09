1a72e937e0a3283bab19c89260032790d9c469a8
talasago <42575556+talasago@users.noreply.github.com>
2024-11-09 20:41:29 +0900

    refactor: update parameter name for context and sort code

--- app/functions/lambda_handler/add_comment.py
+++ app/functions/lambda_handler/add_comment.py
@@ -4,10 +4,9 @@ from app.repository.retrospective_method.comment_repository import CommentReposi
 from app.schemas.retrospective_method.comment_schema import CommentSchema
 
 
-def lambda_handler(event, context, db=get_db()):
-    # TODO: 例外の時どうする？
+def lambda_handler(event, _context, db=get_db()):
     print(f"Received data: {event}")
+    comment = CommentSchema(**event)
     comment_repo = CommentRepository(db)
-    comment = CommentSchema(**event)  # デコレータでもいいかも。
 
     comment_repo.save(CommentModel(**comment.model_dump()))
