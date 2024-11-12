39fb8901f3f6fccfe066c1979639e1a357889d96
talasago <42575556+talasago@users.noreply.github.com>
2024-11-12 21:51:19 +0900

    Revert "feat(handler comment): updated that function can receive DI"
    This reverts commit 0f984b526ee4868d4a21c56e128b6ac9c552f6cd.

--- app/functions/lambda_handler/add_comment.py
+++ app/functions/lambda_handler/add_comment.py
@@ -3,8 +3,10 @@ from app.models.retrospective_method.comment_model import CommentModel
 from app.repository.retrospective_method.comment_repository import CommentRepository
 from app.schemas.retrospective_method.comment_schema import CommentSchema
 
+db = get_db()
 
-def lambda_handler(event, context, db=get_db()):
+
+def lambda_handler(event, context):
     # TODO: 例外の時どうする？
     print(f"Received data: {event}")
     comment_repo = CommentRepository(db)
