e2b230cb7127535fc2a40775a4742623f59f5021
talasago <42575556+talasago@users.noreply.github.com>
2024-11-12 21:51:15 +0900

    Revert "refactor: update parameter name for context and sort code"
    This reverts commit 1a72e937e0a3283bab19c89260032790d9c469a8.

--- app/functions/lambda_handler/add_comment.py
+++ app/functions/lambda_handler/add_comment.py
@@ -4,9 +4,10 @@ from app.repository.retrospective_method.comment_repository import CommentReposi
 from app.schemas.retrospective_method.comment_schema import CommentSchema
 
 
-def lambda_handler(event, _context, db=get_db()):
+def lambda_handler(event, context, db=get_db()):
+    # TODO: 例外の時どうする？
     print(f"Received data: {event}")
-    comment = CommentSchema(**event)
     comment_repo = CommentRepository(db)
+    comment = CommentSchema(**event)  # デコレータでもいいかも。
 
     comment_repo.save(CommentModel(**comment.model_dump()))
