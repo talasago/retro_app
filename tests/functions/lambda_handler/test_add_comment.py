82c2740491eb5b217956704087f221e9f236ec6b
talasago <42575556+talasago@users.noreply.github.com>
2024-11-12 21:51:21 +0900

    Revert "test: fix retrospective_method_id"
    This reverts commit 7b98e915294aee24f34d2a1c252635caa3072675.

--- tests/functions/lambda_handler/test_add_comment.py
+++ tests/functions/lambda_handler/test_add_comment.py
@@ -10,6 +10,7 @@ from tests.test_helpers.create_test_user import create_test_user
 def sut():
     def _method(event: dict, context: dict):
         return lambda_handler(event, context)
+
     return _method
 
 
@@ -18,8 +19,8 @@ def test_lambda_handler(sut, monkeypatch, db, user_repo):
 
     user = create_test_user(user_repo, name="test_lambda_handler")
     event = {
-        "comment": "test comment test_lambda_handler",
-        "retrospective_method_id": 56,
+        "comment": "test comment",
+        "retrospective_method_id": 5,
         "user_id": user.id,
     }
     sut(event, {})
