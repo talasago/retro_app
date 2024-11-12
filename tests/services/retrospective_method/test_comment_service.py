5594078cca0504520091003510d58f45863e5ae9
talasago <42575556+talasago@users.noreply.github.com>
2024-11-12 21:51:22 +0900

    Revert "test: refactor test"
    This reverts commit 246ea8065daee31a3614285c6460e08dde38a2ac.

--- tests/services/retrospective_method/test_comment_service.py
+++ tests/services/retrospective_method/test_comment_service.py
@@ -1,3 +1,4 @@
+# test_sut.py
 import json
 
 import boto3
@@ -9,38 +10,34 @@ from app.services.retrospective_method.comment_service import CommentService
 
 
 class TestCommentService:
-    @pytest.fixture(scope="session")
+    @pytest.fixture
     def mock_sfn_client(self):
         with mock_aws():
             client = boto3.client("stepfunctions", region_name="ap-northeast-1")
-            yield client
 
-    @pytest.fixture(scope="session")
-    def create_sfn(self, mock_sfn_client):
-        def _method():
-            # 本当はsls.ymlから取得したい
             state_machine_definition = {
-                 "StartAt": "AddCommentState",
-                    "States": {
-                        "AddCommentState": {
-                            "Type": "Task",
-                            "Resource": "arn:aws:lambda:ap-northeast-1:000000000000:function:add_comment_function",
-                            "End": True,
+                "StartAt": "AddCommentState",
+                "States": {
+                    "AddCommentState": {
+                        "Type": "Task",
+                        "Resource": "arn:aws:lambda:ap-northeast-1:000000000000:function:add_comment_function",
+                        "End": True,
                     }
                 },
             }
 
-            response = mock_sfn_client.create_state_machine(
+            response = client.create_state_machine(
                 name="AddCommentStateMachine",
                 definition=json.dumps(state_machine_definition),
                 roleArn="arn:aws:iam::000000000000:role/StateMachineRole",
             )
-            return response
-        return _method
 
-    @pytest.fixture(scope="session")
-    def sut(self, mock_sfn_client, create_sfn):
-        return CommentService(mock_sfn_client, create_sfn()["stateMachineArn"])
+            yield client, response["stateMachineArn"]
+
+    @pytest.fixture
+    def sut(self, mock_sfn_client):
+        client, state_machine_arn = mock_sfn_client
+        return CommentService(client, state_machine_arn)
 
     class TestAddCommentFromApi:
         # 仮のテスト
