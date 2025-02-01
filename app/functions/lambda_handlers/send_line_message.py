126a8ce8b781e7d7cf5d1ae503520ddf9c1a995b
talasago <42575556+talasago@users.noreply.github.com>
2025-02-01 19:15:17 +0900

    fix: add print statement to output JSON data in send_line_message.py

--- app/functions/lambda_handlers/send_line_message.py
+++ app/functions/lambda_handlers/send_line_message.py
@@ -8,6 +8,7 @@ from app.services.notification_service import NotificationService
 def lambda_handler(event, context):
     decoded_data = base64.b64decode(event["awslogs"]["data"])
     json_data = json.loads(gzip.decompress(decoded_data))
+    print(json_data)
 
     # 2回以上同じログの入力があるかわかっていない。一応配列なのでforで回す
     for log in json_data["logEvents"]:
