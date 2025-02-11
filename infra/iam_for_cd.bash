8b99ce2b8bfcb6fc803cece264773336b3ecf94d
talasago <42575556+talasago@users.noreply.github.com>
2025-02-11 18:19:08 +0900

    feat: update iam_for_cd script to accept multiple arguments and use environment variables for role names

--- infra/iam_for_cd.bash
+++ infra/iam_for_cd.bash
@@ -3,26 +3,36 @@
 # $ export AWS_PROFILE='[YOUR PROFILE NAME]'
 
 usage() {
-    echo "Usage: $0 arg1"
-    echo "arg1:"
+    echo "Usage: $0 arg4"
+    echo "arg4:"
     echo "  create, stack create,"
     echo "  update, stack update."
     echo "  delete, stack delete."
 }
 
-if [[ $# != 1 ]]; then
+if [[ $# != 4 ]]; then
   usage;
   exit 1;
 fi
 
-readonly TEMPLATE_FILE_NAME='iam_for_cd.yml'
 readonly STACK_NAME='iam-for-cd'
 
-case $1 in
+
+# 変数の設定
+export MIGRATE_ROLE_NAME="$1"
+export DEPLOY_ROLE_NAME="$2"
+export POLICY_NAME="$3"
+readonly OUTPUT_FILE="./iam_for_cd_generated.yml"
+
+# テンプレートファイルを読み込み、変数を置換して出力ファイルに書き込む
+envsubst < './iam_for_cd.yml' > $OUTPUT_FILE
+
+
+case $4 in
   create)
-    aws cloudformation create-stack --stack-name "${STACK_NAME}" --template-body "file://${TEMPLATE_FILE_NAME}"  --capabilities CAPABILITY_NAMED_IAM;;
+    aws cloudformation create-stack --stack-name "${STACK_NAME}" --template-body "file://${OUTPUT_FILE}"  --capabilities CAPABILITY_NAMED_IAM;;
   update)
-    aws cloudformation update-stack --stack-name "${STACK_NAME}" --template-body "file://${TEMPLATE_FILE_NAME}"  --capabilities CAPABILITY_NAMED_IAM;;
+    aws cloudformation update-stack --stack-name "${STACK_NAME}" --template-body "file://${OUTPUT_FILE}"  --capabilities CAPABILITY_NAMED_IAM;;
   delete)
     aws cloudformation describe-stack-events --stack-name "${STACK_NAME}";;
   *)
